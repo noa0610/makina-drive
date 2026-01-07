f69d1d0cfbf80d5c1cfb765376408202ed8c440c
noa0610 <human.kagoshima.yamanaka@gmail.com>
2026-01-07 16:11:05 +0900

    [add] エフェクト制作（スナイパー弾、経験値、回復アイテム）

--- makina-drive/Assets/02_Scripts/Item/ExpItem.cs
+++ makina-drive/Assets/02_Scripts/Item/ExpItem.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 // 経験値アイテム
 public class ExpItem : DropItem
 {
-    private float _expAmount;
+    [SerializeField] private float _expAmount;
     public void SetExp(float amount) => _expAmount = amount;
 
     protected override void OnCollect(UnitBase target)
